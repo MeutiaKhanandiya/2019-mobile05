1b3a1339705bfe2e2cd9c1866ee46ec01580d5ca
MeutiaKhanandiya <khanandyam@gmail.com>
2019-09-29 00:22:04 +0700

    menyelesaikan Pengenalan Fragment sampai Fragment Transition

--- app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
@@ -2,7 +2,13 @@ package polinema.ac.id.starterchapter05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import polinema.ac.id.starterchapter05.Activities.DynamicActivity;
+import polinema.ac.id.starterchapter05.Activities.PraktikumActivity;
+import polinema.ac.id.starterchapter05.Activities.StaticActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +17,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void handlerPraktikumFragment(View view) {
+        Intent intent = new Intent (this, PraktikumActivity.class) ;
+        startActivity(intent) ;
+    }
+
+    public void handlerDynamicFragment(View view) {
+        Intent intent = new Intent (this, DynamicActivity.class) ;
+        startActivity(intent);
+    }
+
+    public void handlerStaticFragment(View view) {
+        Intent intent = new Intent (this, StaticActivity.class) ;
+        startActivity(intent) ;
+    }
+
+
 }
